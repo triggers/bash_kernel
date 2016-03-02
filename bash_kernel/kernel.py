b2730ccf1cd92a3e8f150d0e0848e3d1ada4aa4a
Richard Potter <potter@axsh.net>
2016-03-02 20:44:12 +0900

    Implement continuous output by making a subclass of REPLWrapper

--- bash_kernel/kernel.py
+++ bash_kernel/kernel.py
@@ -1,5 +1,6 @@
 from ipykernel.kernelbase import Kernel
 from pexpect import replwrap, EOF
+import pexpect
 
 from subprocess import check_output
 from os import unlink
@@ -9,6 +10,7 @@ import imghdr
 import re
 import signal
 import urllib
+import os
 
 __version__ = '0.2'
 
@@ -18,6 +20,31 @@ from .images import (
     extract_image_filenames, display_data_for_image, image_setup_cmd
 )
 
+class CREPLWrapper(replwrap.REPLWrapper):
+    def __init__(self, cmd_or_spawn, orig_prompt, prompt_change,
+                 new_prompt=replwrap.PEXPECT_PROMPT,
+                 continuation_prompt=replwrap.PEXPECT_CONTINUATION_PROMPT,
+                 extra_init_cmd=None, bkernelp=None):
+        self.bkernel = bkernelp
+        replwrap.REPLWrapper.__init__(self, cmd_or_spawn, orig_prompt, prompt_change,
+                                      new_prompt, continuation_prompt, extra_init_cmd)
+
+    def _expect_prompt(self, timeout=-1):
+        if timeout == None:
+            # Only one run_command below uses timeout=None, and it should receive continous output
+            while True:
+                pos = self.child.expect_exact([self.prompt, self.continuation_prompt, '\r\n'],
+                                              timeout=None)
+                if pos == 2:
+                    # if end of line, immediately send output so far
+                    self.bkernel.process_output(self.child.before + '\n')
+                else:
+                    break
+        else:
+            # The other run_commands use other timeout values, and all output should be collected
+            pos = self.child.expect_exact([self.prompt, self.continuation_prompt],
+                                          timeout=None)
+        return pos
 
 class BashKernel(Kernel):
     implementation = 'bash_kernel'
@@ -52,15 +79,40 @@ class BashKernel(Kernel):
         # so that bash and its children are interruptible.
         sig = signal.signal(signal.SIGINT, signal.SIG_DFL)
         try:
-            self.bashwrapper = replwrap.bash()
+            bashrc = os.path.join(os.path.dirname(pexpect.__file__), 'bashrc.sh')
+            child = pexpect.spawn("bash", ['--rcfile', bashrc], echo=False,
+                                  encoding='utf-8')
+            self.bashwrapper = CREPLWrapper(child,
+                                            u'\$', u"PS1='{0}' PS2='{1}' PROMPT_COMMAND=''",
+                                            extra_init_cmd="export PAGER=cat", bkernelp=self)
         finally:
             signal.signal(signal.SIGINT, sig)
 
         # Register Bash function to write image data to temporary file
         self.bashwrapper.run_command(image_setup_cmd)
 
+    def process_output(self, output):
+        if not self.silent:
+            image_filenames, output = extract_image_filenames(output)
+
+            # Send standard output
+            stream_content = {'name': 'stdout', 'text': output}
+            self.send_response(self.iopub_socket, 'stream', stream_content)
+
+            # Send images, if any
+            for filename in image_filenames:
+                try:
+                    data = display_data_for_image(filename)
+                except ValueError as e:
+                    message = {'name': 'stdout', 'text': str(e)}
+                    self.send_response(self.iopub_socket, 'stream', message)
+                else:
+                    self.send_response(self.iopub_socket, 'display_data', data)
+
+        
     def do_execute(self, code, silent, store_history=True,
                    user_expressions=None, allow_stdin=False):
+        self.silent = silent
         if not code.strip():
             return {'status': 'ok', 'execution_count': self.execution_count,
                     'payload': [], 'user_expressions': {}}
@@ -77,22 +129,7 @@ class BashKernel(Kernel):
             output = self.bashwrapper.child.before + 'Restarting Bash'
             self._start_bash()
 
-        if not silent:
-            image_filenames, output = extract_image_filenames(output)
-
-            # Send standard output
-            stream_content = {'name': 'stdout', 'text': output}
-            self.send_response(self.iopub_socket, 'stream', stream_content)
-
-            # Send images, if any
-            for filename in image_filenames:
-                try:
-                    data = display_data_for_image(filename)
-                except ValueError as e:
-                    message = {'name': 'stdout', 'text': str(e)}
-                    self.send_response(self.iopub_socket, 'stream', message)
-                else:
-                    self.send_response(self.iopub_socket, 'display_data', data)
+        self.process_output(output)
 
         if interrupted:
             return {'status': 'abort', 'execution_count': self.execution_count}
